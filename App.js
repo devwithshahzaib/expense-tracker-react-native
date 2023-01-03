2e81c2933313f69f4067df1fc808c353d68b5687
devwithshahzaib <shahzaib191102@gmail.com>
2023-01-03 14:22:06 +0500

    initializing project

---
+++ App.js
@@ -0,0 +1,20 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function App() {
+  return (
+    <View>
+      <Text
+        style={{
+          backgroundColor: 'grey',
+          color: 'white',
+          fontSize: 30,
+          fontWeight: 'bold',
+          padding: 10,
+          textAlign: 'center',
+        }}>
+        Shah Zaib
+      </Text>
+    </View>
+  );
+}
